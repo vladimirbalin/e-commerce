2303159fa59b946552cda47642eda13872c39343
vladimirB <vladimirzq@gmail.com>
2020-10-14 00:24:41 +0600

    file structure and smarty template engine setup

---
+++ tmp/smarty/templates_c/e28123203a22631b835f9c4f1b4418747222db2d_0.file.index.tpl.php
@@ -0,0 +1,39 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-10-14 00:22:48
+  from 'D:\web-no-sync\openserver\OpenServer\domains\e-commerce\views\default\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5f85f078185948_97049088',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e28123203a22631b835f9c4f1b4418747222db2d' => 
+    array (
+      0 => 'D:\\web-no-sync\\openserver\\OpenServer\\domains\\e-commerce\\views\\default\\index.tpl',
+      1 => 1602613226,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5f85f078185948_97049088 (Smarty_Internal_Template $_smarty_tpl) {
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $_smarty_tpl->tpl_vars['pageTitle']->value;?>
+</title>
+</head>
+<body>
+    <?php echo $_smarty_tpl->tpl_vars['pageTitle']->value;?>
+
+</body>
+</html><?php }
+}
